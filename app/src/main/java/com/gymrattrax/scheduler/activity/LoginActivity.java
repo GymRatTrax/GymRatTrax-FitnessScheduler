525ea021d23ba0d4c386df0f64025bb9888c9e58
christopher-stone <christopher@closetoeuphoria.com>
2015-06-29 13:18:13 -0400

    LoginActivity updates (still needs testing)

--- app/src/main/java/com/gymrattrax/scheduler/activity/LoginActivity.java
+++ app/src/main/java/com/gymrattrax/scheduler/activity/LoginActivity.java
@@ -1,35 +1,45 @@
-/*
- * ******************************************************************************
- *  * Copyright (c) 2014 Gabriele Mariotti.
- *  *
- *  * Licensed under the Apache License, Version 2.0 (the "License");
- *  * you may not use this file except in compliance with the License.
- *  * You may obtain a copy of the License at
- *  *
- *  * http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  * See the License for the specific language governing permissions and
- *  * limitations under the License.
- *  *****************************************************************************
- */
-
 package com.gymrattrax.scheduler.activity;
 
+import android.content.Intent;
+import android.content.IntentSender;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.games.Games;
-import com.gymrattrax.scheduler.BuildConfig;
+import com.gymrattrax.scheduler.R;
 
-public class LoginActivity extends LoginBaseActivity {
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
     public static final String TAG = "LoginActivity";
+    /**
+     * Google API client.
+     */
+    protected GoogleApiClient mGoogleApiClient;
+    /**
+     * Request code for auto Google Play Services error resolution.
+     */
+    protected static final int REQUEST_CODE_RESOLUTION = 1;
+    // Bool to track whether the app is already resolving an error
+    private boolean mIntentInProgress;
 
-    protected boolean mClientConnected = false;
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Fitness.HISTORY_API).addScope(Fitness.SCOPE_ACTIVITY_READ_WRITE)
+                .addApi(Fitness.SESSIONS_API).addScope(Fitness.SCOPE_LOCATION_READ_WRITE)
+                .addApi(Fitness.RECORDING_API).addScope(Fitness.SCOPE_BODY_READ_WRITE)
+                .addApi(Games.API).addScope(Games.SCOPE_GAMES)
+                .build();
+    }
     /**
      * Called when activity gets visible. A connection to Drive services need to
      * be initiated as soon as the activity is visible. Registers
@@ -37,26 +47,83 @@ public class LoginActivity extends LoginBaseActivity {
      * activities itself.
      */
     @Override
-    protected void onResume() {
-        if (BuildConfig.DEBUG_MODE) Log.v(TAG, "onResume()");
-        super.onResume();
-        //TODO: Handle mGoogleApiClient better
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(Fitness.HISTORY_API).addScope(Fitness.SCOPE_ACTIVITY_READ_WRITE)
-                    .addApi(Fitness.SESSIONS_API).addScope(Fitness.SCOPE_LOCATION_READ_WRITE)
-                    .addApi(Fitness.RECORDING_API).addScope(Fitness.SCOPE_BODY_READ_WRITE)
-                    .addApi(Games.API).addScope(Games.SCOPE_GAMES)
-                    .build();
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    /**
+     * Called when activity gets invisible.
+     */
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home_screen, menu);
+        return true;
+    }
+
+    /**
+     * Called when {@code mGoogleApiClient} is trying to connect but failed.
+     * Handle {@code result.getResolution()} if there is a resolution is
+     * available.
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.i(TAG, "GoogleApiClient connection failed: " + result.toString());
+        if (!mIntentInProgress) { // Not already attempting to resolve an error...
+            if (!result.hasResolution()) {
+                mIntentInProgress = true;
+                // show the localized error dialog.
+                GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), this, 0).show();
+                return;
+            }
+            try {
+                mIntentInProgress = true;
+                result.startResolutionForResult(this, REQUEST_CODE_RESOLUTION);
+            } catch (IntentSender.SendIntentException e) {
+                Log.e(TAG, "Exception while starting resolution activity", e);
+                mIntentInProgress = false;
+                mGoogleApiClient.connect();
+            }
+        }
+    }
+
+    /**
+     * Called when {@code mGoogleApiClient} is connected.
+     */
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Log.i(TAG, "GoogleApiClient is connected!");
+    }
+
+    /**
+     * Called when {@code mGoogleApiClient} is disconnected.
+     */
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.i(TAG, "GoogleApiClient connection suspended");
         mGoogleApiClient.connect();
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        if (BuildConfig.DEBUG_MODE) Log.v(TAG, "onPause() - not implemented");
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_RESOLUTION) {
+            mIntentInProgress = false;
+            if (resultCode == RESULT_OK) {
+                // Make sure the app is not already connected or attempting to connect
+                if (!mGoogleApiClient.isConnecting() && !mGoogleApiClient.isConnected()) {
+                    mGoogleApiClient.connect();
+                }
+            }
+        }
     }
 }
